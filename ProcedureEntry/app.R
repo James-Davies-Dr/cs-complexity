72fcdafeac3729ea95d08c4e88b900721656ab08
James-Davies-Dr <james.m.j.davies@googlemail.com>
2022-05-04 20:36:40 +0100

    definitely broke something

--- ProcedureEntry/app.R
+++ ProcedureEntry/app.R
@@ -54,7 +54,7 @@ ui <- dashboardPage(
               titlePanel(h1("Upload a new case")),
               box(
                   textInput("PatientID", label = "Patient ID"), 
-                  dateInput("date", label = "Date of CS", format = "dd-M-yyyy", startview = "month"),
+                  dateInput("Date", label = "Date of CS", format = "dd-M-yyyy", startview = "month"),
                   numericInput("BMI", label = "BMI", value = 25, min = 15, max = 100, step = 1),
                   numericInput("Age", label = "Maternal Age", value = 30, min = 15, max = 60, step = 1),
                   numericInput("Parity", label = "Parity", value = 0, min = 0, max = NA, step = 1),
@@ -66,19 +66,25 @@ ui <- dashboardPage(
               box(numericInput("PreviousCS", label = "Previous CS", value = 0, min = 0, max = NA, step = 1),
                   textAreaInput("PreviousSurgery", label = "Any Previous Surgery excluding CS"),
                   radioButtons("FullDilatation", label = "Fully Dilated?", choices = c("Yes", "No"), selected = "No"),
-                  radioButtons("LowerSegmentFibroids", label = "Fibroids in lower segment?", choices = c("Yes", "No"), selected = "No"),
-                  radioButtons("Multiple", label = "Multiple Pregnancy", choices = c("Singleton", "DCDA", "MCDA", "MCMA", "Triplets or More"), selected = "Singleton"),
+                  radioButtons("LowerSegmentFibroids", label = "Fibroids in lower segment?",
+                               choices = c("Yes", "No"), selected = "No"),
+                  radioButtons("Multiple", label = "Multiple Pregnancy",
+                               choices = c("Singleton", "DCDA", "MCDA", "MCMA", "Triplets or More"), 
+                               selected = "Singleton"),
                   width = 4),
               box(radioButtons("Placenta", label = "Placental Location", 
                                choices = c("High", "Low anterior", "Low but not anterior", "Complete Praevia",
                                            "Accreta", "Increta", "Percreta"), selected = "High"),
                   radioButtons("Lie", label = "Fetal Lie", choices = c("Longitudinal", "Oblique", "Transverse")),
-                  radioButtons("Presentation", label = "Presentation", choices = c("Cephalic", "Breech", "Brow", "Face", "Shoulder", "Arm")),
+                  radioButtons("Presentation", label = "Presentation",
+                               choices = c("Cephalic", "Breech", "Brow", "Face", "Shoulder", "Arm")),
                   
                   
-                  numericInput("BloodLoss", label = "Blood loss to nearest 100mls", value = 500, min = 0, max = NA, step = 100),
+                  numericInput("BloodLoss", label = "Blood loss to nearest 100mls", 
+                               value = 500, min = 0, max = NA, step = 100),
                   width = 4),
-              actionButton("Submit", label = "Upload")
+              actionButton("button", label = "Upload"),
+              tableOutput(table)
               
               )
               
@@ -93,37 +99,10 @@ ui <- dashboardPage(
     
   
 
-server <- function(input, output) {
+server <- function(input, output, session) {
+ 
   
-####function to show our data thus far.####
-
-  sheet_id <- "https://docs.google.com/spreadsheets/d/1l3FpvszyA464lUdF8flhxtJc7L3f6SCgSlVQyn43GrE/edit#gid=0"
-  read_sheet(sheet_id) -> cs_data  
-  
-  output$cs_data_table <-render_gt({
-    cs_data %>% 
-      gt()
-    })
-  
-####collect responses into a dataframe when pressing submit####
-   
-  
-      
-   observeEvent(input$Submit, {
-     df <- reactive({
-       BMI =  input$BMI
-     }
-     )}
-   )
-  
-}               
-               
-               
-               
-             
-  
-  
-
+}
 
 
 
--- testbed.r
+++ testbed.r
@@ -11,7 +11,7 @@ sheet_id <- "https://docs.google.com/spreadsheets/d/1l3FpvszyA464lUdF8flhxtJc7L3
 read_sheet(sheet_id) -> cs_data
 
 
-cs_data %>% gt()
+cs_data 
 
 test_upload <- data.frame(
   LogbookID = 1,
@@ -38,5 +38,60 @@ sheet_append(sheet_id, test_upload, sheet = 1)
 ##next is creating the shiny thing that gets those answers from the shiny inputs; plus  a box to clear the inputs, and box to append, lets make an easier one to start with.
 
 
+)
 
+v
 
+PatientID = character(),
+Date = character(),
+BMI = character(),
+Age = character(),
+Parity = character(),
+Gestation = character(),
+Indication = character(),
+PreviousCS = character(),
+PreviousSurgery = character(),
+FullDilatation  = character(),
+LowerSegmentFibroids  = character(),
+Multiple  = character(),
+Placenta  = character(),
+Lie = character(),
+Presentation  = character(),
+BloodLoss = character())
+
+####function to show our data thus far.####
+
+sheet_id <- "https://docs.google.com/spreadsheets/d/1l3FpvszyA464lUdF8flhxtJc7L3f6SCgSlVQyn43GrE/edit#gid=0"
+read_sheet(sheet_id) -> cs_data  
+
+output$cs_data_table <-render_gt({
+  cs_data %>% 
+    gt()
+})
+
+####collect responses into a dataframe when pressing submit####
+
+rv <- reactiveValues()
+
+observeEvent(input$Submit, {
+  df <- as.data.frame(
+    
+}
+  )
+  
+  sheet_append(sheet_id, df, sheet = 1)
+  PatientId = input$PatientID()
+  Date = input$Date()
+  Indication = input$Indication()
+  BMI = input$BMI()
+  PreviousCS = input$PreviousCS()
+  PreviousSurgery = input$PreviousSurgery()
+  Placenta = input$Placenta()
+  Lie =  input$Lie()
+  Presentation = input$Presentation()
+  Multiple = input$Multiple()
+  Gestation = input$Gestation()
+  FullDilatation = input$FullDilataion()
+  LowerSegmentFibroids = input$LowerSegmentFibroids()
+  BloodLoss = input$BloodLoss()  
+  
